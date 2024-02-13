54868ec9a2d918650fa967ea5ff7d8017a3708e0
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-02-13 09:30:05 -0600

    initial commit

---
+++ backCifrado/src/main/java/com/bondis/backCifrado/BackCifradoApplication.java
@@ -0,0 +1,13 @@
+package com.bondis.backCifrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCifradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCifradoApplication.class, args);
+	}
+
+}
