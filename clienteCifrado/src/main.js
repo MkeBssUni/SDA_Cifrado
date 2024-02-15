f46513fc6b701d5ba54ec857b09e440f18de5a5a
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-02-15 09:24:36 -0600

    [ADD] config frontend project

--- clienteCifrado/src/main.js
+++ clienteCifrado/src/main.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+//axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+//Axios
+Vue.use(VueAxios, axios)
 
 import './assets/main.css'
 
