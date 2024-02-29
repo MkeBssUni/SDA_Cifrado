b1bea693801c26775981c1fd7d4b7589ac9b72d0
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-02-29 01:57:52 -0600

    cleaning code

--- cifrado/src/main/java/com/bondis/cifrado/modules/hash/service/HashService.java
+++ cifrado/src/main/java/com/bondis/cifrado/modules/hash/service/HashService.java
@@ -11,41 +11,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Base64;
-//
-//@Service
-//public class HashService {
-//
-//    @Value("${hash.secret}")
-//    private String secretKey;
-//    private static final String ALGORITHM = "AES";
-//
-//    public String encrypt(String plaintext) throws Exception {
-//        SecretKey key = generateSecretKeyFromString();
-//        Cipher cipher = Cipher.getInstance(ALGORITHM);
-//        cipher.init(Cipher.ENCRYPT_MODE, key);
-//        byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes());
-//        return Base64.getEncoder().encodeToString(encryptedBytes);
-//    }
-//
-//    public String decrypt(String encryptedText) throws Exception {
-//        SecretKey key = generateSecretKeyFromString();
-//        Cipher cipher = Cipher.getInstance(ALGORITHM);
-//        cipher.init(Cipher.DECRYPT_MODE, key);
-//        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
-//        return new String(decryptedBytes);
-//    }
-//    public SecretKey generateSecretKeyFromString() throws NoSuchAlgorithmException, UnsupportedEncodingException {
-//        MessageDigest sha = MessageDigest.getInstance("SHA-256");
-//        byte[] keyBytes = sha.digest(secretKey.getBytes(StandardCharsets.UTF_8));
-//
-//        // Solo utilizar los primeros 128 bits para claves AES de 128 bits
-//        keyBytes = Arrays.copyOf(keyBytes, 16);
-//
-//        // Crear y devolver la SecretKey
-//        return new SecretKeySpec(keyBytes, "AES");
-//    }
-//}
-
 import javax.crypto.spec.IvParameterSpec;
 
 @Service
